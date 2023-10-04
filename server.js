585efb765d10859c535134baff532f2279bc1131
JASON_LUI <Jason53lui@gmail.com>
2023-10-03 20:29:07 -0700

    Changed the port on the bottom to try to fix render

--- server.js
+++ server.js
@@ -89,6 +89,6 @@ const server = http.createServer(function (req, res) {
   }
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => console.log(`Server Listening on ${PORT}`))
